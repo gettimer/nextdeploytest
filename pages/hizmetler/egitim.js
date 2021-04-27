5c3ac3b721aa9b0867c53eebcb37b540dbfaf5a9
uzeyiroztemur <uzeyir.oztemur@terapiyazilim.com>
2021-04-27 17:37:50 +0300

    eğitim hizmetleri içeriği eklendi.

--- pages/hizmetler/egitim.js
+++ pages/hizmetler/egitim.js
@@ -46,12 +46,31 @@ export default function Hizmetler() {
             <div className='container'>
                 <div className={styles.split_content_view}>
                     <div className={styles.content}>
-                        <h3>Eğitim Hizmetlerimiz İle Yanınızdayız...</h3>
-                        <p>Terapi Yazılım, deneyimli ve konusunda uzman sertifikalı kadrosuyla iş ortaklarının, bilgi altyapılarında meydana gelen sorun ve ihtiyaçlara anında çözüm geliştirmek amacıyla geniş bir yelpazede eğitim hizmetleri sunmaktadır.</p>
-                        <p>Kurumsal ve bireysel eğitimler ile iş ortaklarının program bilgilerini analiz ederek en verimli şekilde kullanmalarına olanak sağlamaktadır.</p>                        
+                        <h3>Eğitim Hizmetleri</h3>
+                        <p>Logo ve Terapi ürünlerini yeni kullanmaya başlamış, ya da kullanım alanlarında yetersiz olduğunu düşünen kullanıcılarımıza, ihtiyaca ve firma özelliklerine göre uygun çalışma metotları ile özelliklerin sunulduğu eğitim hizmetlerini sağlamaktayız.</p>
+                        <p>“İstek merağı uyandırır, merak ise eğitim ile giderilir.”</p>
+                        <strong>KURUMSAL EĞİTİM HİZMETLERİ</strong>
+                        <br/>
+                        <small>İşinizin ihtiyaçlarına uygun özel içerikli eğitimler veriyoruz.</small><br/><br/>
+                        <p>Logo ve Terapi ürünlerini kapsayan geniş bir içeriğe sahip eğitim programları sunuyoruz. Uzman, deneyimli ve sertifikalı ekibimizle, katılımcılara kullanıcı eğitimleri veya sistem uzmanlık eğitimleri vermekteyiz. İhtiyaçlar doğrultusunda kurumsal verilen eğitimler bireysel olarak da alınabilmektedir.</p>
+                        <p><strong>BİREYSEL EĞİTİMLER</strong></p>
+                        <p>Bir konu ya da iş konusunda öğrenilen ya da öğretilen şeylere “bilgi” denir. Hayatımız boyunca bilgi edinmeye devam ederiz. Gerçek bilgiye sahip olanlar her zaman üstündür. Bireylerin, ister özel, isterse de kurumsal etki alanlarında alacağı profesyonel eğitimler, çok önemli kazanımlar ile birlikte fark yaratmanın temellerini oluşturacaktır.</p>
+                        <p>Başarının temelinde sizlere verilen eğitimlerin hayatınızda kullanımı yatmaktadır. Bu bilgileri kullandığımız sürece eğitilmiş ve uygulamada başaran bireyler ya da kurumlar olacağız. Kişiselleştirilmiş eğitimler işte tam da burada fayda sağlamaktadır. İlerlemek istediğiniz profesyonel iş hayatınızda kişisel gelişiminize veya profesyonelleşmek istediğiniz alanlara odaklanılmış özel eğitimler çok değerlidir.</p>
+                        <p>Dönüşüm, hedef doğrultusunda olduğumuzdan farklı bir duruma değişme biçimidir. </p>
+                        <strong>DÖNÜŞÜMÜ SAĞLAYAN KURUMSAL EĞİTİMLER</strong>
+                        <br/><br/>
+                        <span>1-     Standart kullanıcı modül eğitimleri,</span><br/>
+                        <span>2-     Kurumun sektörüne dayalı veya iş odaklı uzmanlık eğitimleri, </span><br/>
+                        <span>3-     Kişisel, uzmanlık eğitimleri,</span><br/>
+                        <span>4-     Projeye bazlı özel eğitimler,  </span><br/>
+                        <span>5-     Kurumsal raporlama uzmanlık eğitimleri.</span><br/><br/>
+
+                        <p>Bilgiyi, iki şekilde elde edebiliriz; deneme yanılma ya da deneyim transferi ile!</p>
+                        <p>Başlangıç aşamasında deneyim transferi pahalı gibi görünse de, işin sonunda zamandan ve tutardan fayda sağlayacaktır.</p>
+
                     </div>
                     <div className={styles.media}>
-                        <img src='/img/about.jpg' />
+                        <img src='/img/services/education.jpg' className={styles.pl_pr_60} />
                     </div>
                 </div>
 
